ee1d3aaf6dea4cf212597c119f5e597123df3e9a
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2023-11-08 22:47:26 +0300

    style: 🎨✨ Update `Prettier` configurations and upgrade `prettier-plugin-tailwindcss` version

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import { ThemeToggle } from '@/components';
 
 export const Header = () => (
-  <header className='flex w-full items-center justify-between bg-neutral-200 px-4 py-3 dark:bg-gray-900'>
+  <header className='flex w-full items-center justify-between bg-neutral-200 px-4 py-3 dark:bg-gray-900 '>
     <div className='flex gap-5 '>
       <span className='text-2xl'>Velenyx</span>
       <div className='flex items-center gap-3'>
