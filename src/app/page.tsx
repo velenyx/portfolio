72dbecd377389d5e20d9e7259d4d6aed44ce261b
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2023-11-09 23:40:37 +0300

    fix: 🎨 Adjust image dimensions and margin in `page.tsx`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,18 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className='flex items-center'>
-      <div>
+      <div className='pr-6'>
         <h2 className='pb-2 text-4xl font-bold'>Dmitriy Sivritkin</h2>
         <p>
           🌌 I am Frontend Developer (TypeScript, React, Next). Сaffeine converter to React&apos;s
           magic. 💫
         </p>
       </div>
+
       <Image
         width='100'
         height='100'
-        className='ml-6 rounded-full border-2 border-neutral-100 object-cover'
+        className='rounded-full border-2 border-neutral-100 object-cover'
         src='/images/me.jpg'
         alt="Sivritkin's Dmitriy Image"
       />
