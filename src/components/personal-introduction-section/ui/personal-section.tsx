e45158f194e44f754b200c25e4fbcc36a4837e0e
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2023-11-13 22:48:03 +0300

    fix: :bug: Correct flex property in `PersonalIntroductionSection` and enhance description text

--- src/components/personal-introduction-section/ui/personal-section.tsx
+++ src/components/personal-introduction-section/ui/personal-section.tsx
@@ -1,10 +1,13 @@
 import { ProfileImage } from './profile-image';
 
 export const PersonalIntroductionSection = () => (
-  <section className='flex animate-slide-up flex-wrap items-center justify-center duration-200 sm:flex-nowrap'>
+  <section className='flex animate-slide-up flex-wrap items-center justify-between duration-200 sm:flex-nowrap'>
     <div itemScope itemType='http://schema.org/Person'>
       <h1 className='pb-2 text-4xl font-bold'>Dmitriy Sivritkin</h1>
-      <p>🌌 I am Frontend Developer...</p>
+      <p>
+        🌌 I am Frontend Developer (TypeScript, React, Next). Сaffeine converter to React&apos;s
+        magic. 💫
+      </p>
     </div>
     <ProfileImage />
   </section>
